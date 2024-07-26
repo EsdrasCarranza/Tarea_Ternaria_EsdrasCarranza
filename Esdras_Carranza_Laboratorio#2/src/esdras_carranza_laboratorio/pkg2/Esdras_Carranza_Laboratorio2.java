139224a75400dc5b4f2c9eb9119b72a373075b0c
50488 <50488@192.168.0.19>
2024-07-26 08:33:46 -0600

    Inicio de lab 2

---
+++ Esdras_Carranza_Laboratorio#2/src/esdras_carranza_laboratorio/pkg2/Esdras_Carranza_Laboratorio2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package esdras_carranza_laboratorio.pkg2;
+
+/**
+ *
+ * @author 50488
+ */
+public class Esdras_Carranza_Laboratorio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
